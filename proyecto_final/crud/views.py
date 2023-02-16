08d48073968d7a1f7b549d47e1eb1996bf0e9659
Iván Betanzos Macías <ivan.betanzos89@gmail.com>
2023-02-16 05:13:43 +0100

    fix update methods

--- proyecto_final/crud/views.py
+++ proyecto_final/crud/views.py
@@ -73,6 +73,26 @@ class JuegoViewSet(BaseViewSet):
             return Response(serializer.data)
         
         return Response(serializer.error_messages, status=STATUS['BAD_REQUEST'])
+    
+    def partial_update(self, request, pk=None):
+        queryset = self.model_class.objects.get(pk=pk)
+        data = request.data
+        
+        if 'id_plataforma' in data and not isinstance(data['id_plataforma'], int):
+            if isinstance(data['id_plataforma'], str):
+                plataforma, created = models.Plataforma.objects.get_or_create(nombre=data['id_plataforma'])
+                data['id_plataforma'] = plataforma.id
+            if isinstance(data['id_plataforma'], dict):
+                plataforma, crated = models.Plataforma.objects.get_or_create(nombre=data['id_plataforma']['nombre'])
+                data['id_plataforma'] = plataforma.id
+                
+        serializer = self.serializer_class(
+            queryset, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=STATUS['BAD_REQUEST'])
+        
 
     def seed_data_base(self, request):
         self.model_class.objects.all().delete()
@@ -159,3 +179,48 @@ class MensajeViewSet(BaseViewSet):
         
         return Response(serializer.errors, status=STATUS['BAD_REQUEST'])
     
+    def partial_update(self, request, pk=None):
+        queryset = self.model_class.objects.get(pk=pk)
+        data = request.data
+        
+        #red social
+        if 'id_red_social' in data and not isinstance(data['id_red_social'], int):
+            if isinstance(data['id_red_social'], str):
+                red_social, created = models.Red_social.objects.get_or_create(nombre=data['id_red_social'])
+                data['id_red_social'] = red_social.id
+            if isinstance(data['id_red_social'], dict):
+                red_social, created = models.Red_social.objects.get_or_create(nombre=data['id_red_social']['nombre'], url=data['id_red_social']['url'])
+                data['id_red_social'] = red_social.id
+                
+        #usuario
+        if 'id_usuario' in data and not isinstance(data['id_usuario'], int):
+            if isinstance(data['id_usuario'], str):
+                usuario = models.Usuario.objects.get(nick=data['id_usuario'])
+                if usuario == None:
+                    raise Exception('No se encuentra el usuario')
+                data['id_usuario'] = usuario.id
+            if isinstance(data['id_usuario'], dict):
+                usuario = models.Usuario.objects.get(nick=data['id_usuario']['nick'])
+                if usuario == None:
+                    raise Exception('No se encuentra el usuario')
+                data['id_usuario'] = usuario.id
+                
+        #juego
+        if 'id_juego' in data and not isinstance(data['id_juego'], int):
+            if isinstance(data['id_juego'], str):
+                juego = models.Juego.objects.get(titulo=data['id_juego'])
+                if juego == None:
+                    raise Exception("El juego no existe")
+                data['id_juego'] = juego.id
+            if isinstance(data['id_juego'], dict):
+                juego = models.Juego.objects.get(titulo=data['id_juego']['titulo'])
+                if juego == None:
+                    raise Exception("El juego no existe")
+                data['id_juego'] = juego.id
+        
+        serializer = self.serializer_class(
+            queryset, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=STATUS['BAD_REQUEST'])
